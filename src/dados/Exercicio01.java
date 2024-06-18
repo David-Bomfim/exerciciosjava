68605d6c5162f4d1a818ce8ae9f94fb0ca3a5c03
David Bomfim <davidrochabomfim6@gmail.com>
2024-06-18 17:25:46 -0300

    Novos Exercicios resolvidos

---
+++ src/dados/Exercicio01.java
@@ -0,0 +1,28 @@
+package dados;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		int y;
+
+		System.out.println("Digite dois valores inteiros: ");
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		int soma = x + y;
+		
+		System.out.println("A Soma dos dois valores inteiros é:  " + soma );
+		
+		sc.close();
+
+		
+	}
+
+}
---
+++ src/dados/Exercicio02.java
@@ -0,0 +1,31 @@
+package dados;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double raio;
+	    double pi = 3.14159;
+	  
+	    System.out.println("Digite o valor do Raio de um Círculo: ");
+	    raio = sc.nextDouble();
+	    
+	    double elevacao = Math.pow(raio,2);
+	    double resultado = pi * elevacao;
+	    
+	    System.out.printf("%.4f%n", resultado );
+	    
+	    sc.close();
+	    
+       
+		
+		
+		
+	}
+
+}
